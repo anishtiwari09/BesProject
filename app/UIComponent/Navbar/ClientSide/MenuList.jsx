2c287d156a89e88b0f8ccf6b1ce8d7980b9ba2e8
Anish Tewari <aneeshtiwari24@gmail.com>
2024-03-18 18:06:50 +0530

    add usememo

--- app/UIComponent/Navbar/ClientSide/MenuList.jsx
+++ app/UIComponent/Navbar/ClientSide/MenuList.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState,useMemo } from "react";
 import MenuCompoenent from "./MenuComponent";
 import { isMobile } from "../../../Utility/device";
 import MobileMenuList from "./MobileMenu/MobileMenuList";
