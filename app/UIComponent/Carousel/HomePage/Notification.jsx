a01a068d4b518537fd214c9583361aa3b63efc5b
unknown <aneeshtiwari24@gmail.com>
2024-05-09 02:10:45 +0530

    add bes report pdf

--- app/UIComponent/Carousel/HomePage/Notification.jsx
+++ app/UIComponent/Carousel/HomePage/Notification.jsx
@@ -30,7 +30,7 @@ export default function Notification() {
             Broadcast & Media Technology
           </h4>
           <h3 className="font-bold text-4xl text-white text-center m-auto mt-6 conference_text conference_text2">
-          {HOMEPAGE.expoStartDate.date}th to {HOMEPAGE.expoEndDate.date}th{" "}
+            {HOMEPAGE.expoStartDate.date}th to {HOMEPAGE.expoEndDate.date}th{" "}
             {HOMEPAGE.expoEndDate.fullMonth} {HOMEPAGE.expoEndDate.year}
           </h3>
         </React.Fragment>
@@ -43,6 +43,22 @@ export default function Notification() {
         Evolving Media Ecosystem: Innovative, Immersive & Sustainable
         Broadcasting
       </div> */}
+      <div className="mt-4 flex gap-2 justify-center">
+        <a
+          className="block w-fit py-[10px] px-3 bg-[#ffcc00] border-box  hover:bg-[#ec008c] delay-[300ms] transition-all text-black text-[15px] cursor-pointer hover:text-white"
+          target="_blank"
+          href="/pdf/others/bes_report_2024.pdf"
+        >
+          POST BES EXPO 2024 REPORT
+        </a>
+        <a
+          className="block w-fit py-[10px] px-3 bg-[#ffcc00] border-box  hover:bg-[#ec008c] delay-[300ms] transition-all text-black text-[15px] cursor-pointer hover:text-white uppercase"
+          target="_blank"
+          href="/pdf/others/bes_event_presentation_2024.pdf"
+        >
+          BES Expo 2024 Presentation
+        </a>
+      </div>
       <h2 className="mt-4 font-bold text-4xl text-center m-auto text-white conference_text ">
         {" "}
         Book Your Space
