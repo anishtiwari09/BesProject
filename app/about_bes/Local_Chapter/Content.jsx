d669a3967da3ffd819ee779e0d1b0c8417fb6ca0
Anish Tewari <aneeshtiwari24@gmail.com>
2024-05-29 14:13:22 +0530

    change

--- app/about_bes/Local_Chapter/Content.jsx
+++ app/about_bes/Local_Chapter/Content.jsx
@@ -18,8 +18,8 @@ export default function Content({ db }) {
         "flex flex-col max-w-[800px] m-auto p-4 " + styles.content_container
       }
     >
-      <h2 className="text-[24px] font-bold">Local Chapter</h2> 
-      <h4 className="text-[14px] font-bold">The Society has 12 Local Chapters</h4>
+     <center> <h2 className="text-[24px] font-bold">Local Chapter</h2> 
+      <h4 className="text-[14px] font-bold">The Society has 12 Local Chapters</h4> </center>
       
 
       <div>
